fe08519a0ddc203e2ca28d10e938ce62e1995101
root <perpetualy@outlook.com>
2018-07-12 12:58:50 +0000

    init httpd

---
+++ html/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Another SSD VPS from Vultr.com!</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<style>
+			a,
+			a:link,
+			a:visited,
+			a:active,
+			a:hover {
+				text-decoration: none;
+			}
+		</style>
+	</head>
+	<body style="position: relative; min-height: 100%; margin: 0px; padding: 0px; background-color: #0b4182; font-family: Arial, sans-serif;">
+		<div style="position: absolute; top: 0px; left: 0px; width: 100%; min-height: 100%; background: linear-gradient(0deg, #0b4182 1%, #1e88e5 100%);">
+
+			<div style="margin-top: 150px; text-align: center;">
+				<a href="https://www.vultr.com/">
+					<img src="logo.png" alt="Vultr" style="width: 100%; min-width: 100px; max-width: 502px; height: auto; padding: 16px; box-sizing: border-box;"/><br/>
+					<span style="text-decoration: none; color: #ffffff; font-size: 24px;">The Infrastructure Cloud™</span>
+				</a>
+			</div>
+
+		</div>
+	</body>
+</html>
